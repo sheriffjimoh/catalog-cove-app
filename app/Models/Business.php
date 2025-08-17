398ce025fa8a4a499a767acaf6e879e3446cc032
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-08-17 07:00:48 +0100

    feat: added business migration and model

---
+++ app/Models/Business.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Business extends Model
+{
+    protected $fillable = [
+        'user_id', 'name', 'whatsapp', 'email',
+        'logo', 'address', 'short_note'
+    ];
+
+
+
+
+    public function owner() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
